a7e9766e8b431c5f9deeb0d506e033e9b8fc272e
tanne <twmerc9411@ung.edu>
2018-10-20 21:28:31 -0400

    I combined Tyler's login page to the working traffic light from Colten.

---
+++ src/edu/ung/mccb/csci/csci3300/iGoFast/Controller/Controller.java
@@ -0,0 +1,185 @@
+package edu.ung.mccb.csci.csci3300.iGoFast.Controller;
+
+import edu.ung.mccb.csci.csci3300.iGoFast.Model.Admin;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+
+import java.nio.charset.Charset;
+import java.security.SecureRandom;
+import java.util.Random;
+import java.util.Timer;
+
+public class Controller {
+
+    @FXML
+    TextField txtadminID, txtusername, txtpassword, txtcpassword;
+    static final String AlphaNum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    static SecureRandom randomSalt = new SecureRandom();
+
+    String genreateRandomSalt( int len ){
+        StringBuilder sb = new StringBuilder(len);
+        for( int i = 0; i < len; i++ )
+            sb.append( AlphaNum.charAt( randomSalt.nextInt(AlphaNum.length()) ));
+        return sb.toString();
+    }
+    public void touchLogin(ActionEvent actionEvent) throws Exception{
+
+        Admin model = new Admin();
+        String userPassword =  txtpassword.getText();
+        String confirmUserPassword =  txtcpassword.getText();
+        boolean isValid= validatePassword(userPassword, confirmUserPassword);
+        if (isValid) {
+            String salt = genreateRandomSalt(100);
+            String hashAndSaltedPassword = model.generateSaltedHashedPassword(userPassword, salt);
+
+            String adminID = generateAdminID();
+            int result = model.saveUserIntoDatabase(adminID, txtusername.getText(), hashAndSaltedPassword, salt);
+            //System.out.println("The salted hash code for the plaintext " + txtpassword.getText() + " is " + hashAndSaltedPassword);
+            Stage primaryStage= new Stage();
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("edu/ung/mccb/csci/csci3300/iGoFast/View/login.fxml"));
+            primaryStage.setTitle("Admin Login");
+            primaryStage.setScene(new Scene(root, 425, 400));
+            primaryStage.show();
+
+
+        }
+        else
+        {
+            message ();
+        }
+
+    }
+
+    public String generateAdminID()
+    {
+        String randomValues = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < 8) {
+            int index = (int) (rnd.nextFloat() * randomValues.length());
+            salt.append(randomValues.charAt(index));
+        }
+        String ID = salt.toString();
+
+        System.out.println("Your Admin ID is " + ID + "\nYou Will Need This For Future Logins");
+        return ID;
+    }
+
+    private static boolean validatePassword(String password, String cPassword) {
+        if (password.equals(cPassword)) {
+            // Regular expression to validate password.
+            if ((password.matches("^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).*$")))
+                return true;
+            else
+                return false;
+        } else {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Password mismatch");
+            alert.setHeaderText("Please re-enter the password");
+            alert.setContentText("The Password must mach, please Please re-enter the password");
+            return false;
+        }
+    }
+    private void message ()
+    {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Password Requirements");
+        alert.setHeaderText("The password entered here  is invalid");
+        alert.setContentText("The Password should be at least 8 characters long.\n" +
+                "Password should contain at least one lowercase letter .\n" +
+                "Password should contain at least one uppercase letter .\n" +
+                "Password should contain at least one digit .\n" +
+                "Password should have at least special character.\n ");
+
+
+        alert.showAndWait();
+
+    }
+
+    public void authenticateUser(ActionEvent actionEvent) {
+        Admin model = new Admin();
+        boolean isRegistered = model.verifyLogin(txtpassword.getText(), txtadminID.getText());
+        if(isRegistered) {
+            System.out.println("Registered user");
+            try {
+                Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("edu/ung/mccb/csci/csci3300/iGoFast/View/light.fxml"));
+                Scene register_page = new Scene(root);
+                Stage app_stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+                app_stage.setScene(register_page);
+                app_stage.show();
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @FXML
+    public void goRegister(ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("edu/ung/mccb/csci/csci3300/iGoFast/View/register.fxml"));
+            Scene register_page = new Scene(root);
+            Stage app_stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            app_stage.setScene(register_page);
+            app_stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+//    @FXML
+    public void goLight(){
+//        try {
+//            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("edu/ung/mccb/csci/csci3300/iGoFast/View/light.fxml"));
+//            Scene register_page = new Scene(root);
+//            Stage app_stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+//            app_stage.setScene(register_page);
+//            app_stage.show();
+//        } catch(Exception e) {
+//            e.printStackTrace();
+//        }
+    }
+
+
+    Timer time = new Timer();
+    @FXML
+    Circle greenlight, yellowlight, redlight;
+    public void ChangeLight() {
+
+        if(greenlight.getOpacity()==1){
+            greenToYellow();
+        }
+        else if (yellowlight.getOpacity()==1) {
+            try {
+                Thread.sleep(3000);
+                yellowToRed();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        else if (redlight.getOpacity()==1){
+            redToGreen();
+        }
+    }
+
+    public void greenToYellow() {
+        greenlight.setOpacity(0);
+        yellowlight.setOpacity(1);
+    }
+    public void yellowToRed() {
+        yellowlight.setOpacity(0);
+        redlight.setOpacity(1);
+    }
+    public void redToGreen() {
+        redlight.setOpacity(0);
+        greenlight.setOpacity(1);
+    }
+}
